51cb4509f18d675d95a50f97da19a77f5eb771c4
Peleg Adiv <peleg912@gmail.com>
2021-04-26 13:09:40 +0300

    make it better

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const getData = async(period, precision)=> {
     try {
-        console.log(`${process.env.REACT_APP_URL}&period=${period}&Precision=${precision}&${process.env.REACT_APP_PARAMS}`);
-    const data = await axios.get(`${process.env.REACT_APP_URL}&period=${period}&Precision=${precision}&${process.env.REACT_APP_PARAMS}`);
+    // const data= await axios.get(`${process.env.REACT_APP_URL}${period}&Precision=${precision}&${process.env.REACT_APP_PARAMS}`);
+    const data = await axios.get(`https://www.fxempire.com/api/v1/en/stocks/chart/candles?Identifier=AAPL.XNAS&IdentifierType=Symbol&AdjustmentMethod=All&IncludeExtended=False&period=${period}&Precision=${precision}&StartTime=8/28/2020%2016:0&EndTime=9/4/2020%2023:59&_fields=ChartBars.StartDate,ChartBars.High,ChartBars.Low,ChartBars.StartTime,ChartBars.Open,ChartBars.Close,ChartBars.Volume`);
      data.data.forEach(obj => {
         let index = obj.StartDate.lastIndexOf('/');
         let index_ = obj.StartTime.lastIndexOf(':');
